abbf31269df1478e28213b283d826f343be41aea
Tornike Khabeishvili <tornikekhabeishvili07@gmail.com>
2023-04-05 18:46:36 +0400

    Finished 1.2

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,27 +1,32 @@
-const Header = (course) => {
+const Header = (props) => {
   return (
-    <h1>{course}</h1>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
   )
 }
 
-const Content = (part1, exercises1, part2, exercises2, part3, exercises3) => {
+const Part = (props) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <p>
+      {props.part}, {props.exercises}
+    </p>
   )
 }
-const Total = (exercises1, exercises2, exercises3) => {
+
+const Content = (props) => {
   return (
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
 
@@ -34,12 +39,11 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>  
   )
 }
